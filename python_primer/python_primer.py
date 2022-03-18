f4475549899f2b24d9c79dfc7184025aa4ff3748
Agnaldo Cardoso Junior <acjunior@Agnaldos-MBP.lan>
2022-03-18 18:39:24 +0000

    add python primer

--- python_primer/python_primer.py
+++ python_primer/python_primer.py
@@ -1,14 +1,13 @@
-# R-1.1
+import array as arrayModule
+
 def is_multiple(n, m):
   if m == 0: return False
   return n % m == 0
 
-# R-1.2
 def is_even(k):
   # Cannot use the multiplication, modulo or division operators
   return k & 1 == 0
 
-# R-1.3
 def min_max(data):
   if (len(data) == 1): return data[0]
   min=max=None
@@ -18,24 +17,23 @@ def min_max(data):
     if number < min: min = number
   return (min, max)
 
-# R-1.4
 def squares_less_than(n):
   total=0
   for integer in range(1, n):
     total+= integer * integer
   return total
 
-# R-1.5
 def squares_comprehension(n):
   return sum(n * n for n in range(1, n))
 
-# R-1.6
 def sum_of_odd_squares(n):
   total=0
   for integer in range(1, n):
     if n & 1 == 1: total+= integer * integer
   return total
 
-# R-1.7
 def sum_of_odd_squares_comprehension(i):
   return sum(n * n for n in range(1, i) if i & 1 == 1)
+
+compactArray = arrayModule.array('i', [2,5,6,10,25])
+[print(item) for item in compactArray]
