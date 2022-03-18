c77d8c1bb31f40a8c52a72dc2549ccc396dfbc5d
Agnaldo Cardoso Junior <acjunior@Agnaldos-MBP.lan>
2022-03-18 19:24:01 +0000

    add stacks

---
+++ stacks/array_stack.py
@@ -0,0 +1,40 @@
+class Empty(Exception):
+  pass
+
+class ArrayStack:
+  """LIFO Stack implementation using Python list as underlying storage."""
+  def __init__(self):
+    """Create an empty stack."""
+    self._data = []
+  
+  def __len__(self):
+    """Return the number of elements in the stack."""
+    return len(self._data)
+
+  def is_empty(self):
+    """Return True if the stack is empty."""
+    return len(self._data) == 0
+
+  def push(self, elem):
+    """add element elem to the top of the stack."""
+    self._data.append(elem)
+
+  def top(self):
+    """
+    Return, but do not remove, the element at the top of the stack.
+
+    Raise Empty exception if the stack is empty.
+    """
+    if self.is_empty():
+      raise Empty('Stack is empty')
+    return self._data[-1]
+
+  def pop(self):
+    """
+    Remove and return the element from the top of the stack.
+
+    Raise Empty exception if the stack is empty.
+    """
+    if self.is_empty():
+      raise Empty('Stack is empty')
+    return self._data.pop()
