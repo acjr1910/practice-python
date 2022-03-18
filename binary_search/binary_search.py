4ed594667b84c762fe010f26ce22d1f09d7c8f78
Agnaldo Cardoso Junior <acjunior@Agnaldos-MBP.lan>
2022-03-18 18:39:41 +0000

    add simple binary search

---
+++ binary_search/binary_search.py
@@ -0,0 +1,19 @@
+def binary_search(data, target, low, high):
+  """
+  Returns True if target is found in indicated portion of a Python List
+  The search only considers the portion from data[low] to data[high] inclusive.
+  """
+  if low > high:
+    return False
+  else:
+    mid = (low + high) // 2
+    if target == data[mid]:
+      return True
+    elif target < data[mid]:
+      return binary_search(data, target, low, mid - 1)
+    else:
+      return binary_search(data, target, mid + 1, high)
+
+data = [2,4,5,7,8,9,12,14,17,19,22,25,27,28,33,37]
+search = binary_search(data, 22, 0, len(data) - 1)
+print(search)
