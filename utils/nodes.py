8537b598570c15a343988d22f54c8acdf53614fa
Agnaldo Cardoso Junior <acjunior@Agnaldos-MacBook-Pro.local>
2022-03-24 20:11:40 +0000

    add doubly node (prev, next)

--- utils/nodes.py
+++ utils/nodes.py
@@ -4,3 +4,11 @@ class Singly_Node:
     def __init__(self, element, next):
         self._element = element
         self._next = next
+
+class Doubly_Node:
+    __slots__ = '_element', '_next', '_prev'
+
+    def __init__(self, element, prev, next):
+        self._element = element
+        self._prev = prev
+        self._next = next
