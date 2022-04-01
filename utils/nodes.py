a519137775be65ef2f6bd0d1fae65105fc494e45
Agnaldo Cardoso Junior <acjunior@Agnaldos-MBP.lan>
2022-04-01 20:09:09 +0100

    add BinaryTree Node ADT

--- utils/nodes.py
+++ utils/nodes.py
@@ -5,6 +5,7 @@ class Singly_Node:
         self._element = element
         self._next = next
 
+
 class Doubly_Node:
     __slots__ = '_element', '_next', '_prev'
 
@@ -12,3 +13,13 @@ class Doubly_Node:
         self._element = element
         self._prev = prev
         self._next = next
+
+
+class BinaryTreeNode():
+    __slots__ = '_element', '_parent', '_left', '_right'
+
+    def __init__(self, element, parent=None, left=None, right=None):
+        self._element = element
+        self._parent = parent
+        self._left = left
+        self._right = right
